4b252899737fcc6e0f2921e9832d1f476475774e
Artem Kvitka <kvitkaart554@gmail.com>
2020-10-07 17:34:19 +0300

    Updates

---
+++ precache-manifest.be6d524adb460f2e97227b0f3547b847.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c90b8b7ab99a0969d6a543cd79e70e",
+    "url": "/werdevs-test/index.html"
+  },
+  {
+    "revision": "3684aabaa628fb57f35f",
+    "url": "/werdevs-test/static/css/main.8fc27954.chunk.css"
+  },
+  {
+    "revision": "d6ef3c3ff3583cf61137",
+    "url": "/werdevs-test/static/js/2.5f283d28.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/werdevs-test/static/js/2.5f283d28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3684aabaa628fb57f35f",
+    "url": "/werdevs-test/static/js/main.ad918416.chunk.js"
+  },
+  {
+    "revision": "33d49b8b2038a8c2e962",
+    "url": "/werdevs-test/static/js/runtime-main.963d5a00.js"
+  },
+  {
+    "revision": "37a75b54218416da18cdf1fa0650c569",
+    "url": "/werdevs-test/static/media/background-image.37a75b54.jpg"
+  },
+  {
+    "revision": "c1b2293586edb83bcd635cdb4e299109",
+    "url": "/werdevs-test/static/media/logo.c1b22935.jpg"
+  }
+]);
